a944aa8303810639e7d3fdb31d52cc02834c6a03
FrankDRE0 <132145164+FrankDRE0@users.noreply.github.com>
2026-06-06 13:58:08 +0100

    Update Play.vue

--- frontend/src/console/views/Play.vue
+++ frontend/src/console/views/Play.vue
@@ -783,6 +783,7 @@ onUnmounted(() => {
         class="select-none"
         draggable="false"
         style="height: 100vh; max-height: 100vh; width: auto; object-fit: cover"
+        onerror="/assets/console/default/bezels/gb.png"
       />
     </div>
     <div
